094c95209d6ca71438f43cce51969eadbc32746e
teguhsnts <steguh2903@gmail.com>
2026-07-07 18:40:32 +0700

    Fix: add missing BasePage import in loginPage.js

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -1,35 +1,4 @@
-// const { BasePage } = require('./basePage');
-
-// class LoginPage extends BasePage {
-//   constructor(page) {
-//     super(page);
-
-//     this.usernameInput = page.getByPlaceholder('Username');
-//     this.passwordInput = page.getByPlaceholder('Password');
-//     this.loginButton = page.getByRole('button', { name: 'Login' });
-//     this.errorMessage = page.locator('[data-test="error"]');
-//   }
-
-//   async goto() {
-//     await super.goto('https://www.saucedemo.com');
-//   }
-
-//   async login(username, password) {
-//     await this.fill(this.usernameInput, username);
-//     await this.fill(this.passwordInput, password);
-//     await this.click(this.loginButton);
-//   }
-
-//   async getErrorMessage() {
-//     return this.getText(this.errorMessage);
-//   }
-
-//   async isErrorVisible() {
-//     return this.isVisible(this.errorMessage);
-//   }
-// }
-
-// module.exports = { LoginPage };
+const { BasePage } = require('./basePage');
 
 class LoginPage extends BasePage {
   constructor(page) {
@@ -42,14 +11,21 @@ class LoginPage extends BasePage {
   }
 
   async goto() {
-    const url = process.env.BASE_URL || 'https://www.saucedemo.com';
-    await super.goto(url);
+    await super.goto('https://www.saucedemo.com');
   }
 
   async login(username, password) {
     await this.fill(this.usernameInput, username);
     await this.fill(this.passwordInput, password);
     await this.click(this.loginButton);
+  }
+
+  async getErrorMessage() {
+    return this.getText(this.errorMessage);
+  }
+
+  async isErrorVisible() {
+    return this.isVisible(this.errorMessage);
   }
 }
 
